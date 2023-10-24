a8d9890ed530237815ea85d5eb8c63a425dba548
tamzid620 <tamzid620@gmail.com>
2023-10-24 12:13:07 +0600

    responsive carousel added

--- src/Components/Pages/StudentDetails/StudentDetails.jsx
+++ src/Components/Pages/StudentDetails/StudentDetails.jsx
@@ -1,9 +1,38 @@
-
+import unknownPhoto from "../../../../public/images/bean.png"
 
 const StudentDetails = () => {
     return (
         <div>
-            <h1>studentDetails</h1>
+            <div className="flex items-center justify-center">
+                <div>
+                    <p className="flex justify-center items-center">Student Details</p>
+                        <div className="flex justify-center items-center"><img className='w-[250px] h-[250px] rounded-xl bg-yellow-200' src={unknownPhoto} alt="" /></div>
+                    <div className="flex gap-5">
+                        <div className=" text-lg grid sm: grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5 mt-10">
+                            <div className="leading-10">
+                                <h1><span className="font-bold text-xl">Name:</span> Rowan Atkinson</h1>
+ {/* eslint-disable-next-line react/no-unescaped-entities */}
+                                <h1><span className="font-bold text-xl">Father's Name:</span> David Atkinson</h1>
+                                 {/* eslint-disable-next-line react/no-unescaped-entities */}
+                                <h1><span className="font-bold text-xl">Mother's Name:</span> Shaila Atkinson</h1>
+                                <h1><span className="font-bold text-xl">BirthDate:</span> 19 FEbruary, 1999</h1>
+                                <h1><span className="font-bold text-xl">Email:</span> Rowan89@gmail.com</h1>
+                                <h1><span className="font-bold text-xl">Adress:</span> 79 westgate, melbourne</h1>
+                            </div>
+                            <div className="leading-10">
+                                <h1><span className="font-bold text-xl">Name:</span> Rowan Atkinson</h1>
+ {/* eslint-disable-next-line react/no-unescaped-entities */}
+                                <h1><span className="font-bold text-xl">Father's Name:</span> David Atkinson</h1>
+ {/* eslint-disable-next-line react/no-unescaped-entities */}
+                                <h1><span className="font-bold text-xl">Mother's Name:</span> Shaila Atkinson</h1>
+                                <h1><span className="font-bold text-xl">BirthDate:</span> 19 FEbruary, 1999</h1>
+                                <button></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div></div>
         </div>
     );
 };
--- src/Components/Shared/Banner/Banner.jsx
+++ src/Components/Shared/Banner/Banner.jsx
@@ -1,5 +1,7 @@
-import banner from '../../../../public/images/tree.jpg'
-import { Carousel } from "@material-tailwind/react";
+import TrialSilder from "./TrialSilder";
+
+
+
 
 const Banner = () => {
     return (
@@ -8,35 +10,7 @@ const Banner = () => {
         <marquee className="text-black font-semibold py-2 text-center bg-gray-300">অক্টোবর হতে ডিসেম্বর/২০২৩ এর বেতন আদায়ের তারিখ ৫ম-৯ম শ্রেণির পর্যন্ত। ২০২৩ সালে এসএসসি উর্ত্তীণ শিক্ষার্থীদের একাডেমিক টান্সক্রীপ্ট বিতরণ সংক্রান্ত বিজ্ঞপ্তি।</marquee>
     </div>
    {/* carousel section  */}
-    <Carousel
-      className="rounded-xl relative z-0"
-      navigation={({ setActiveIndex, activeIndex, length }) => (
-        <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2 ">
-          {new Array(length).fill("").map((_, i) => (
-            <span
-              key={i}
-              className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
-                activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
-              }`}
-              onClick={() => setActiveIndex(i)}
-            />
-          ))}
-        </div>
-      )}
-    >
-      <img
-        src={banner} alt="image 1"
-        className="h-full w-full object-cover"
-      />
-      <img
-        src={banner} alt="image 2"
-        className="h-full w-full object-cover"
-      />
-      <img
-       src={banner} alt="image 3"
-        className="h-full w-full object-cover"
-      />
-    </Carousel>
+<TrialSilder/>
     </div>
     );
 };
