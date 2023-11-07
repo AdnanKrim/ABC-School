522c18ca3a0511532dc2913196e6945c702732d5
tamzid620 <tamzid620@gmail.com>
2023-11-07 11:24:07 +0600

    adminpanel root setup complete

--- src/Components/Pages/Payment/AdminPayment.jsx
+++ src/Components/Pages/Payment/AdminPayment.jsx
@@ -1,9 +1,30 @@
+import SearchPanel from "../Dashboard/SearchPanel/SearchPanel";
+import Drawer from "../Dashboard/SearchPanel/Drawer";
 
 const AdminPayment = () => {
     return (
-        <div>
-            <h1>Admin Payment</h1>
+        <div className="flex justify-between">
+        <div className="z-10">
+          <Drawer />
         </div>
+        {/* table div  */}
+        <div className="">
+          <div className="lg:-ms-20">
+            <SearchPanel />
+          </div>
+          <div className="flex justify-center">
+            <div className="mt-20 me-28">
+              {/* AdminStudentInfo section  */}
+              <h1 className="mt-8 text-3xl font-semibold uppercase text-black flex justify-center ">
+                All Student
+              </h1>
+              <hr className="border border-black mb-8" />
+  
+            
+        </div>
+        </div>
+        </div>
+      </div>
     );
 };
 
